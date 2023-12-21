29750771c75c43414c5161dd720f3f3f99fa0a32
wenbo <wwb@echase.cn>
2023-12-21 15:01:27 +0800

    修改gateway支持nacos的配置信息

--- gateway/src/main/java/cn/rl520/cloud/gateway/GatewayApplication.java
+++ gateway/src/main/java/cn/rl520/cloud/gateway/GatewayApplication.java
@@ -1,5 +1,6 @@
 package cn.rl520.cloud.gateway;
 
+import cn.dev33.satoken.SaManager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,6 +10,7 @@ public class GatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
         System.out.println("~~~~网关服务启动成功~~~~");
+        System.out.println("Sa-Token 配置如下：" + SaManager.getConfig());
     }
 
 }

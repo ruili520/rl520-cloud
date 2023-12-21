366671c6ed9046157250bc0536a78f8f3981c2e8
wenbo <wwb@echase.cn>
2023-12-21 16:12:30 +0800

    新增自动发现方法

--- gateway/src/main/java/cn/rl520/cloud/gateway/GatewayApplication.java
+++ gateway/src/main/java/cn/rl520/cloud/gateway/GatewayApplication.java
@@ -1,10 +1,15 @@
 package cn.rl520.cloud.gateway;
 
 import cn.dev33.satoken.SaManager;
+import cn.dev33.satoken.reactor.spring.SaTokenContextRegister;
+import org.springframework.boot.LazyInitializationExcludeFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class GatewayApplication {
 
     public static void main(String[] args) {

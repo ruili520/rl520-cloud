23a7f4dfef573f2a98d8df7667d8732a142b861b
wenbo <wwb@echase.cn>
2023-12-22 14:50:07 +0800

    修改并引入Hutool 5.8.21

--- README.md
+++ README.md
@@ -2,11 +2,15 @@
 
 参考若依微服务内容进行实现，替换若依使用的权限框架 Spring Security 为 sa-token
 
+## 使用技术栈
+### 使用技术栈
+* spring cloud 
+* [若依框架](https://doc.ruoyi.vip/)
+* [sa-token](https://sa-token.cc/)
+* [Hutool](https://www.hutool.cn/)
 
 ### 网关服务
 
-该项目参考若依项目进行改造:
 
-* [若依框架](https://doc.ruoyi.vip/)
-* [sa-token](https://sa-token.cc/)
+
 
--- gateway/src/main/java/cn/rl520/cloud/gateway/controller/TestController.java
+++ gateway/src/main/java/cn/rl520/cloud/gateway/controller/TestController.java
@@ -1,5 +1,6 @@
 package cn.rl520.cloud.gateway.controller;
 
+import cn.hutool.core.date.DateUtil;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +15,6 @@ public class TestController {
 
     @RequestMapping(value = "test",method = RequestMethod.GET)
     public String test(){
-        return "这是服务网关返回的接口数据，当前时间为"+new Date();
+        return "这是服务网关返回的接口数据，当前时间为"+ DateUtil.formatDateTime(new Date());
     }
 }
